71e5a259156c49f75b6e41d6d83f0c49166fbe10
kuhblume <kasumigaura0524@yahoo.co.jp>
2018-03-29 21:39:19 +0900

    http.jsの変更

--- public/js/app.js
+++ public/js/app.js
@@ -12211,79 +12211,16 @@ module.exports = Cancel;
 "use strict";
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_axios__ = __webpack_require__(5);
 /* harmony import */ var __WEBPACK_IMPORTED_MODULE_0_axios___default = __webpack_require__.n(__WEBPACK_IMPORTED_MODULE_0_axios__);
-// import axios from 'axios'
-// window.axios = require('axios');
-// window.axios.defaults.headers.common = {
-//     'X-Requested-With': 'XMLHttpRequest'
-// };
-// axios.defaults.baseURL = 'https://intense-savannah-76949.herokuapp.com/' + 'api';
-//
-// let href = location.href;//urlの取得
-// alert(href);
-// axios.defaults.baseURL = href + 'api';
-//
-// /**
-//  * Responsible for all HTTP requests.
-//  */
-// export default {
-//     request (method, url, data, successCb = null, errorCb = null) {
-//         axios.request({
-//             url,
-//             data,
-//             method: method.toLowerCase()
-//         }).then(successCb).catch(errorCb)
-//     },
-//
-//     get (url, successCb = null, errorCb = null) {
-//         return this.request('get', url, {}, successCb, errorCb)
-//     },
-//
-//     post (url, data, successCb = null, errorCb = null) {
-//         return this.request('post', url, data, successCb, errorCb)
-//     },
-//
-//     put (url, data, successCb = null, errorCb = null) {
-//         return this.request('put', url, data, successCb, errorCb)
-//     },
-//
-//     delete (url, data = {}, successCb = null, errorCb = null) {
-//         return this.request('delete', url, data, successCb, errorCb)
-//     },
-//
-//     /*
-//      * Init the service.
-//      */
-//     init () {
-//
-//         axios.defaults.baseURL = 'https://intense-savannah-76949.herokuapp.com/' + 'api';  //httpsでデータ送信するように指定(未修整)---追記：ここはこのままでよさそう
-//
-//         // Intercept the request to make sure the token is injected into the header.
-//         axios.interceptors.request.use(config => {
-//             config.headers['X-CSRF-TOKEN']     = window.Laravel.csrfToken;
-//             config.headers['X-Requested-With'] = 'XMLHttpRequest';
-//             config.headers['Authorization']    = `Bearer ${localStorage.getItem('jwt-token')}` // jwtauth
-//             return config
-//         })
-//
-//         // Intercept the response and ...
-//         axios.interceptors.response.use(response => {
-//             // ...get the token from the header or response data if exists, and save it.
-//             const token = response.headers['Authorization'] || response.data['token']
-//             if (token) {
-//                 localStorage.setItem('jwt-token', token)
-//             }
-//
-//             return response
-//         }, error => {
-//             // Also, if we receive a Bad Request / Unauthorized error
-//             console.log(error)
-//             return Promise.reject(error)
-//         })
-//
-//     }
-// }
 
+window.axios = __webpack_require__(5);
+window.axios.defaults.headers.common = {
+    'X-Requested-With': 'XMLHttpRequest'
+};
+__WEBPACK_IMPORTED_MODULE_0_axios___default.a.defaults.baseURL = 'https://intense-savannah-76949.herokuapp.com/' + 'api';
 
+var href = location.href; //urlの取得
+alert(href);
+__WEBPACK_IMPORTED_MODULE_0_axios___default.a.defaults.baseURL = href + 'api';
 
 /**
  * Responsible for all HTTP requests.
@@ -12326,20 +12263,83 @@ module.exports = Cancel;
     },
 
 
-    /**
+    /*
      * Init the service.
      */
     init: function init() {
-        __WEBPACK_IMPORTED_MODULE_0_axios___default.a.defaults.baseURL = '/api';
+
+        __WEBPACK_IMPORTED_MODULE_0_axios___default.a.defaults.baseURL = 'https://intense-savannah-76949.herokuapp.com/' + 'api'; //httpsでデータ送信するように指定(未修整)---追記：ここはこのままでよさそう
 
         // Intercept the request to make sure the token is injected into the header.
         __WEBPACK_IMPORTED_MODULE_0_axios___default.a.interceptors.request.use(function (config) {
             config.headers['X-CSRF-TOKEN'] = window.Laravel.csrfToken;
             config.headers['X-Requested-With'] = 'XMLHttpRequest';
+            config.headers['Authorization'] = 'Bearer ' + localStorage.getItem('jwt-token'); // jwtauth
             return config;
+        });
+
+        // Intercept the response and ...
+        __WEBPACK_IMPORTED_MODULE_0_axios___default.a.interceptors.response.use(function (response) {
+            // ...get the token from the header or response data if exists, and save it.
+            var token = response.headers['Authorization'] || response.data['token'];
+            if (token) {
+                localStorage.setItem('jwt-token', token);
+            }
+
+            return response;
+        }, function (error) {
+            // Also, if we receive a Bad Request / Unauthorized error
+            console.log(error);
+            return Promise.reject(error);
         });
     }
 });
+//
+// import axios from 'axios'
+//
+// /**
+//  * Responsible for all HTTP requests.
+//  */
+// export default {
+//     request (method, url, data, successCb = null, errorCb = null) {
+//         axios.request({
+//             url,
+//             data,
+//             method: method.toLowerCase()
+//         }).then(successCb).catch(errorCb)
+//     },
+//
+//     get (url, successCb = null, errorCb = null) {
+//         return this.request('get', url, {}, successCb, errorCb)
+//     },
+//
+//     post (url, data, successCb = null, errorCb = null) {
+//         return this.request('post', url, data, successCb, errorCb)
+//     },
+//
+//     put (url, data, successCb = null, errorCb = null) {
+//         return this.request('put', url, data, successCb, errorCb)
+//     },
+//
+//     delete (url, data = {}, successCb = null, errorCb = null) {
+//         return this.request('delete', url, data, successCb, errorCb)
+//     },
+//
+//     /**
+//      * Init the service.
+//      */
+//     init () {
+//         axios.defaults.baseURL = '/api'
+//
+//         // Intercept the request to make sure the token is injected into the header.
+//         axios.interceptors.request.use(config => {
+//             config.headers['X-CSRF-TOKEN']     = window.Laravel.csrfToken
+//             config.headers['X-Requested-With'] = 'XMLHttpRequest'
+//             return config
+//         })
+//     }
+// }
+//
 
 /***/ }),
 /* 15 */
