213e4b4973c597a05c0eebda196661191306be00
kuhblume <kasumigaura0524@yahoo.co.jp>
2018-04-22 17:49:09 +0900

    app.jpのヘッダ設定？をコメント化

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -1,8 +1,8 @@
 window.axios = require('axios');
 
-window.axios.defaults.headers.common = {
-    'X-Requested-With': 'XMLHttpRequest'
-};
+// window.axios.defaults.headers.common = {
+//     'X-Requested-With': 'XMLHttpRequest'
+// };
 
 import Vue from 'vue'
 import router from './router'
