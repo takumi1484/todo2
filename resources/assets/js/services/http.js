625d61eef9cb57dcc5e00f8b72cb8ed59d9283f7
kuhblume <kasumigaura0524@yahoo.co.jp>
2018-04-22 17:45:07 +0900

    http.jsのinitをコメント化

--- resources/assets/js/services/http.js
+++ resources/assets/js/services/http.js
@@ -40,34 +40,34 @@ export default {
     /*
      * Init the service.
      */
-    init () {
-
-        axios.defaults.baseURL = 'https://intense-savannah-76949.herokuapp.com/' + 'api';  //httpsでデータ送信するように指定(未修整)---追記：ここはこのままでよさそう
-
-        // Intercept the request to make sure the token is injected into the header.
-        axios.interceptors.request.use(config => {
-            config.headers['X-CSRF-TOKEN']     = window.Laravel.csrfToken;
-            config.headers['X-Requested-With'] = 'XMLHttpRequest';
-            config.headers['Authorization']    = `Bearer ${localStorage.getItem('jwt-token')}` // jwtauth
-            return config
-        })
-
-        // Intercept the response and ...
-        axios.interceptors.response.use(response => {
-            // ...get the token from the header or response data if exists, and save it.
-            const token = response.headers['Authorization'] || response.data['token']
-            if (token) {
-                localStorage.setItem('jwt-token', token)
-            }
-
-            return response
-        }, error => {
-            // Also, if we receive a Bad Request / Unauthorized error
-            console.log(error)
-            return Promise.reject(error)
-        })
-
-    }
+    // init () {
+    //
+    //     axios.defaults.baseURL = 'https://intense-savannah-76949.herokuapp.com/' + 'api';  //httpsでデータ送信するように指定(未修整)---追記：ここはこのままでよさそう
+    //
+    //     // Intercept the request to make sure the token is injected into the header.
+    //     axios.interceptors.request.use(config => {
+    //         config.headers['X-CSRF-TOKEN']     = window.Laravel.csrfToken;
+    //         config.headers['X-Requested-With'] = 'XMLHttpRequest';
+    //         config.headers['Authorization']    = `Bearer ${localStorage.getItem('jwt-token')}` // jwtauth
+    //         return config
+    //     })
+    //
+    //     // Intercept the response and ...
+    //     axios.interceptors.response.use(response => {
+    //         // ...get the token from the header or response data if exists, and save it.
+    //         const token = response.headers['Authorization'] || response.data['token']
+    //         if (token) {
+    //             localStorage.setItem('jwt-token', token)
+    //         }
+    //
+    //         return response
+    //     }, error => {
+    //         // Also, if we receive a Bad Request / Unauthorized error
+    //         console.log(error)
+    //         return Promise.reject(error)
+    //     })
+    //
+    // }
 }
 //
 // import axios from 'axios'
